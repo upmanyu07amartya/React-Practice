4eb6120bb26cf4c35e4b541163b09aaabe43f01d
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2024-07-11 09:22:28 +0530

    episode01

---
+++ App.js
@@ -0,0 +1,22 @@
+{
+  /* <div id="parent">
+    <div id = "child">
+        <h1>Hello</h1>
+        <h1>Amartya</h1> hoow to create siblings? In the 3rd argument of its parent, pass both elements in an array
+    </div>
+</div> */
+}
+// Way to create nested elements
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello"),
+    React.createElement("h1", {}, "Amartya"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+console.log("1230", parent);
+ 
