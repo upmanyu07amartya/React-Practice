e46e70ab140156a13be20f33839be7df19935f52
Amartya Upmanyu <upmanyu07amartya@gmail.com>
2024-08-28 23:29:22 +0530

    episode 3 - Laying foundation

--- App.js
+++ App.js
@@ -1,25 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-{
-  /* <div id="parent">
-    <div id = "child">
-        <h1>Hello</h1>
-        <h1>Amartya</h1> hoow to create siblings? In the 3rd argument of its parent, pass both elements in an array
-    </div>
-</div> */
-}
-// Way to create nested elements
+//React Element - React.createElement => object.
+// When we render this obj to DOM, then it becomes a HTML element.
+// JSX - Javascript syntax which is easier to create React elements.
+// JSX is not a part of react.
+// JSX is not HTML inside Javascript. It is like HTML like syntax.
+//browsers cant understand JSX as it is not JS code.
+// JSX is transpiled before going to JS engine using the package Babel
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello"),
-    React.createElement("h1", {}, "Namaste React"),
-    React.createElement("h1", {}, "Amartya"),
-  ])
-);
+//const heading = React.createElement("h1",{id:"heading"},"Namaste React");
+// const jsxHeading = (
+//   <h1 id="heading" className="head">
+//     Namaste React using JSX
+//   </h1>
+// );
+
+const TitleComponent = ()=>{
+    return <h2>Title</h2>
+};
+
+// console.log("heading",heading)
+// console.log("jsx",jsxHeadding)
+
+// These 2 objs are one and the same thing
+
+//REACT COMPONENTS
+// CLASS BASED AND FUNCTION BASED
+
+// React Functional Component - It is a normal JS function which returns some JSX
+
+const HeadingComponent = () => {
+  return (
+    <div>
+      <h1>Namaste React functional component</h1>
+      <TitleComponent />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
-console.log("1230", parent);
+//root.render(jsxHeading) - this is how to render a element
+root.render(<HeadingComponent />); // this is how you render a component
